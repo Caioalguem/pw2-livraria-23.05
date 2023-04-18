5344390e906aba696bfd146acc7165fb05652fb4
Luci Luci <luciluci200-@gmail.com>
2023-04-18 08:56:44 -0300

    USO DO PRIVATE

--- src/LivroTeste.java
+++ src/LivroTeste.java
@@ -3,24 +3,24 @@ public class LivroTeste {
     public static void main(String[] args) {
 
         Livro meuLivro = new Livro( "O Hobbit",8.99);
-        meuLivro.paginas = 521;
-        meuLivro.resumo = "bla bla bla bla bla";
-        meuLivro.autor = "J. R. Tolkien";
+        meuLivro.setPaginas(521);
+        meuLivro.setResumo ("bla bla bla bla bla");
+        meuLivro.setAutor("J. R. Tolkien");
 
         Editora editora1 = new Editora("Editora Erica");
         //editora1.nome ="Editora Erica";
         editora1.email = "edErica@gmail.com";
-        meuLivro.editora = editora1;
+        meuLivro.setEditora (editora1);
 
-        meuLivro.aplicarDesconto(0.1);
+        meuLivro.aplicarDesconto(0.2);
 
         meuLivro.exibirDados();
 
         Livro livroFavorito = new Livro( "O Código Da Vinci",8.99);
-        livroFavorito.resumo = "bla bla bla bla bla";
-        livroFavorito.paginas = 500;
-        livroFavorito.autor = "Dan Brown";
-
+        livroFavorito.setResumo ("bla bla bla bla bla");
+        livroFavorito.setPaginas (500);
+        livroFavorito.setAutor("Dan Brown");
+        livroFavorito.setTipoCapa(TipoCapaEnum.PERSONALIZADA);
         livroFavorito.aplicarDesconto(0.25);
 
         livroFavorito.exibirDados();
