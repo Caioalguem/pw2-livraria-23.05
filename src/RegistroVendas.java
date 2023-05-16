7c8a76cd458be7f859a7479e0f4fc1ab2774c774
Luci Luci <luciluci200-@gmail.com>
2023-05-16 08:54:03 -0300

    pw2-livraria-2.0

--- src/RegistroVendas.java
+++ src/RegistroVendas.java
@@ -1,15 +1,9 @@
 public class RegistroVendas {
     private double total;
 
-    public void adicionar(LivroFisico lf){
-        System.out.println("Livro adicionado: " + lf.getNome());
-        lf.aplicarDesconto(0.10);
-        total = total + lf.getPreco();
-    }
-
-    public void adicionar(LivroDigital lf){
-        System.out.println("Livro adicionado: " + lf.getNome());
-        lf.aplicarDesconto(0.10);
-        total = total + lf.getPreco();
+    public void adicionar(Livro Livro){
+        System.out.println("Livro adicionado: " + Livro.getNome());
+        Livro.aplicarDesconto(0.10);
+        total = total + Livro.getPreco();
     }
 }
